3882fa45f759cf1af828c6d5e484ff64b69123e7
Student <student@PNCB12-22.pnc.lan>
2021-02-25 15:20:48 +0700

    create background

--- Game.py
+++ Game.py
@@ -11,36 +11,23 @@ canvas=tk.Canvas(frame)
 color={"blue","red","green","purple","yello"}
 
 # Verrible
+# Game-Over= False
+oval1=canvas.create_rectangle(0,0,600,600, fill="yellow")
+oval2=canvas.create_rectangle(0,0,600,20, fill="black")
+oval3=canvas.create_rectangle(0,0,20,600, fill="black")
+oval4=canvas.create_rectangle(580,600,600,0, fill="black")
+oval4=canvas.create_rectangle(0,580,600,600, fill="black")
+snake=canvas.create_oval(50,50,80,80, fill="cyan")
+snake=canvas.create_oval(60,50,70,60, fill="black")
 
 
 
-canvas.create_rectangle(0,0,0,0, fill="yellow")
-# canvas.create_rectangle(0,20,20,600, fill="teal")
-# canvas.create_rectangle(0,600,20,600, fill="teal")
-# canvas.create_rectangle(0,600,20,600, fill="teal")
-# FUNCTIONS
-
-
-
-
-# convas.creat_oval(12,14,39,30)
-# # display
+# display
 frame.pack(expand=True, fill="both")
 canvas.pack(expand=True, fill='both')
 root.mainloop()
 
 
-
-
-
-
-# for row in range(10):
-#     x1=0
-#     y1=0
-#     x2=x1+50
-#     y2=x2+50
-#     for column in range(10):
-#         canvas.create_rectangle(x1,y1,x2,y2, fill="teal")
 
 
 
